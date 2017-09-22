ef1a208e4f521a8dc410dae21b6d7fb75cf3c0e5
Matías Herranz <matiasherranz@gmail.com>
2017-09-22 12:29:48 -0300

    Added a db.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
+var pg = require('pg');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -28,5 +30,17 @@ app.get('/times', function(request, response) {
     for (i=0; i < times; i++)
       result += i + ' ';
   response.send(result);
+});
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
 });
 
