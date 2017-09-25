6b4412b6711f6c755797bb26c542092b271ed461
Matías Herranz <matiasherranz@gmail.com>
2017-09-25 15:16:22 -0300

    Updated index.js.

--- index.js
+++ index.js
@@ -1,46 +1,177 @@
-var cool = require('cool-ascii-faces');
-var express = require('express');
-var pg = require('pg');
+// Required imports
+const express = require('express'); // Express web server framework
+const request = require('request'); // "Request" library
+const querystring = require('querystring');
+const cookieParser = require('cookie-parser');
+const cool = require('cool-ascii-faces');
+const pg = require('pg');
 
-var app = express();
+// Constants
+const port = process.env.PORT || 5000;
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+const redirect_uri = process.env.REDIRECT_URI || `http://localhost:${port}/callback`;
+const stateKey = 'spotify_auth_state';
 
-app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+// Initialize the app
+const app = express();
 
-// views is directory for all template files
+// Setup the app
+app.set('port', port);
+
+app.use(express.static(__dirname + '/public'))
+   .use(cookieParser());
+
+// Set the directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+const generateRandomString = (length) => {
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let text = '';
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+
+// URLs config
 app.get('/', function(request, response) {
-  response.render('pages/index')
+  response.render('pages/index', { ascii_face: cool() })
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
+
+app.get('/login', function(req, res) {
+
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  // your application requests authorization
+  const scope = 'user-read-private user-read-email';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
 
-app.get('/times', function(request, response) {
-    var result = ''
-    var times = process.env.TIMES || 5
-    for (i=0; i < times; i++)
-      result += i + ' ';
-  response.send(result);
-});
+app.get('/callback', function(req, res) {
 
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+
+  const code = req.query.code || null;
+  const state = req.query.state || null;
+  const storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    res.clearCookie(stateKey);
+    const authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        const access_token = body.access_token;
+        const refresh_token = body.refresh_token;
+
+        const options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        request.get(options, function(error, response, body) {
+          console.log(body);
+            pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+              client.query(
+                'INSERT into spotify_access (access_token, refresh_token) VALUES($1, $2)',
+                [access_token, refresh_token], function(err, result) {
+                done();
+                if (err)
+                 { console.error(err); res.send("Error " + err); }
+                else {
+                  console.log('all OK');
+                  res.render(
+                    'pages/logged_in',
+                    { access_token, refresh_token, data: body }
+                  );
+
+                }
+              });
+            });
+        });
+
+      } else {
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
     });
+  }
+});
+
+
+app.get('/refresh_token', function(req, res) {
+
+  // requesting access token from refresh token
+  var refresh_token = req.query.refresh_token;
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
+      res.send({
+        'access_token': access_token
+      });
+    }
   });
 });
+
+
+// Start the app
+app.listen(port, () => {
+  console.log('Node app is running on port', port);
+});
+
+
+
 
