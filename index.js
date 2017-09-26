7a33c2cb99e39a3978be4e44934526d29597c739
Matías Herranz <matiasherranz@gmail.com>
2017-09-25 22:00:49 -0300

    Lots of changes on index.js.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Required imports
 const express = require('express'); // Express web server framework
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const request = require('request'); // "Request" library
 const path = require('path');
 const querystring = require('querystring');
-const cookieParser = require('cookie-parser');
 const cool = require('cool-ascii-faces');
 const pg = require('pg');
 
@@ -21,8 +22,38 @@ const app = express();
 // Setup the app
 app.set('port', port);
 
+// Setup the middleware
+
+let whitelist = [
+  'https://ipsyfy.herokuapp.com',
+  'http://ipsyfy.herokuapp.com',
+];
+
+if (process.env.NODE_ENV !== 'production') {
+  whitelist = whitelist.concat([
+    'http://localhost:3000',
+    'http://localhost:3001',
+  ]);
+}
+
+const corsOptions = {
+  origin(origin, callback) {
+    // origin is undefined for non-cross-origin requests
+    // See here for reference: http://stackoverflow.com/a/15988671/1860704
+    const isLocalRequest = (origin === undefined);
+    const isChromeExtension = !isLocalRequest && origin.indexOf('chrome-extension://') !== -1;
+    const isCorsOk = whitelist.indexOf(origin) !== -1;
+    const originIsWhitelisted = isLocalRequest || isCorsOk || isChromeExtension;
+    if (!originIsWhitelisted) {
+      logger.error(`The request is not whitelisted: ${origin}`);
+    }
+    callback(originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted);
+  },
+};
+
 app.use(express.static(__dirname + '/public'))
-   .use(cookieParser());
+   .use(cookieParser())
+   .use(cors(corsOptions));
 
 // Set the directory for all template files
 app.set('views', __dirname + '/views');
@@ -50,9 +81,6 @@ app.get('/', function(request, response) {
   response.render('pages/index', { ascii_face: cool() })
 });
 
-app.get('/search', function(request, response) {
-  response.sendFile(path.resolve('public', 'fe', 'index.html'));
-});
 
 app.get('/login', function(req, res) {
 
@@ -124,7 +152,7 @@ app.get('/callback', function(req, res) {
                 if (err)
                  { console.error(err); res.send("Error " + err); }
                 else {
-                  console.log('all OK');
+                  console.log('Done saving tokens to db');
                   res.render(
                     'pages/logged_in',
                     { access_token, refresh_token, data: body }
@@ -149,8 +177,8 @@ app.get('/callback', function(req, res) {
 app.get('/refresh_token', function(req, res) {
 
   // requesting access token from refresh token
-  var refresh_token = req.query.refresh_token;
-  var authOptions = {
+  const refresh_token = req.query.refresh_token;
+  const authOptions = {
     url: 'https://accounts.spotify.com/api/token',
     headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
     form: {
@@ -162,11 +190,42 @@ app.get('/refresh_token', function(req, res) {
 
   request.post(authOptions, function(error, response, body) {
     if (!error && response.statusCode === 200) {
-      var access_token = body.access_token;
-      res.send({
-        'access_token': access_token
-      });
+      const access_token = body.access_token;
+      res.send({ access_token });
     }
+  });
+});
+
+app.get('/cool', (request, response) => response.send(cool()));
+
+
+// Endpoint to serve the ReactJS frontend
+app.get('/search', (request, response) => {
+  response.sendFile(path.resolve('public', 'fe', 'index.html'));
+});
+
+
+app.get('/get-tokens', (request, response) => {
+  const q = `
+    SELECT access_token, refresh_token
+    FROM spotify_access
+    ORDER BY created_at DESC LIMIT 1
+  `;
+
+  pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+    client.query(q, (err, result) => {
+      done();
+      if (err) {
+        console.error(err);
+        response.send("Error: " + err);
+      } else {
+        const data = JSON.stringify(result.rows[0]);
+        console.log('db query result: ', data);
+
+        response.setHeader('Content-Type', 'application/json');
+        response.send(data);
+      }
+    });
   });
 });
 
@@ -175,7 +234,3 @@ app.get('/refresh_token', function(req, res) {
 app.listen(port, () => {
   console.log('Node app is running on port', port);
 });
-
-
-
-
